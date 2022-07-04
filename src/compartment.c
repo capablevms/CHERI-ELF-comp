5d1261db8ccc2b5118a5bcb4ff0718f401340fa0
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2022-07-04 14:31:36 +0100

    Remove unneeded code

--- src/compartment.c
+++ src/compartment.c
@@ -307,8 +307,7 @@ comp_add_intercept(struct Compartment* new_comp, uintptr_t intercept_target, str
     // `ldr c6, [x6, $OFFSET]`
     const int32_t arm_ldr_instr_mask = 0b1100001001 << 22; // includes 0b00 bits for `op` field
     ptraddr_t arm_ldr_pcc_offset = comp_manager_cap_addr; // offset within 4KB page
-    ptraddr_t offset_correction = align_down(comp_manager_cap_addr, 4096);
-    ptraddr_t offset_correction2 = align_down(comp_manager_cap_addr, 1 << 12);
+    ptraddr_t offset_correction = align_down(comp_manager_cap_addr, 1 << 12);
     arm_ldr_pcc_offset -= offset_correction;
 
     assert(arm_ldr_pcc_offset < 65520); // from ISA documentation
