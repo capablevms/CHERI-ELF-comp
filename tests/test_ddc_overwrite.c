cb33f46cfa3852981b9b2e1aa492b46912793607
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2023-10-17 15:36:24 +0100

    Add new test and rework testing interface
    * Add a test `ddc_overwrite`, which attempts to break the DDC
      compartmentalizatin approach by temporarily using the PCC as the DDC
      to access a secret
    * Simplify testing interfact in `tests` CMakeFile. Now it's easier to
      write how tests will be ran, without a bunch of in-built inference

---
+++ tests/test_ddc_overwrite.c
@@ -0,0 +1,36 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "cheriintrin.h"
+
+/* This test atempts to circumvent the DDC compartmentalization feature by
+ * using the existing PCC as a temporary DDC to access a secret pointer beyond
+ * the original bounds of the compartment. The address of the secret is
+ * expected to be passed as `secret_addr`, and the function will return the
+ * value at that address. If the given `secret_addr` is out of the original DDC
+ * bounds, and this test does not fail, then we have broken the expected
+ * security guarantees of our DDC compartmentalization approach.
+ */
+
+int
+test_leak(unsigned long long secret_addr)
+{
+    int secret;
+    asm volatile("cvtp c0, %[addr]\n\t"
+                 "ldr %w[val], [c0]"
+            : [val] "=r"(secret)
+            : [addr] "r"(secret_addr)
+            : "x0", "memory");
+    return secret;
+}
+
+int
+main()
+{
+    int* secret = malloc(sizeof(int));
+    *secret = 42;
+    int val = test_leak((unsigned long long) secret);
+    assert(val == *secret);
+    free(secret);
+    return 0;
+}
